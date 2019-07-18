1f0ef84e636e946ca83442cedc24bfe75ae0bd28
Shubham Singh <shubhamsinghstw@gmail.com>
2019-07-18 18:12:19 +0530

    Final commit

---
+++ Bootstrap4/conFusion/js/interactivity.js
@@ -0,0 +1,11 @@
+/*function show()
+{
+	var x,y;
+	x=document.getElementById('options');
+	y=document.getElementById('check');
+	if(y.checked){
+		x.className=show;
+	}
+	else
+		x.className=notshow;
+}*/
